828b38e5935030bd9b764da384b50009e3146b6f
Ayush raj anand <rajanand.ayush98@gmail.com>
2020-12-31 22:20:02 +0530

    Added Rest apis

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,27 @@
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => {
+      return (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          markComplete={this.props.markComplete}
+          delTodo={this.props.delTodo}
+        />
+      );
+    });
+  }
 }
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
 
 export default Todos;
